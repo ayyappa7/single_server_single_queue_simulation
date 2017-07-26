6416a2b17f100423b0f32c2df01ae623a86e8918
ayyappa7 <ayyappa715@gmail.com>
2017-07-26 22:21:34 +0530

    Main Class

---
+++ ModS.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mods;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author Ayyappa
+ */
+public class ModS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        int i;
+        Onetrial ot = new Onetrial();
+      //  for(i=0;i<60;i++)
+       // ot.Averagewt();
+         
+        // TODO code application logic here
+    }
+}
